67b4a78b982195fa67dead466c19caee4fdb9be8
Dan Winter <dan.j.winter@gmail.com>
2016-01-05 17:04:00 -0700

    Implement users, still need to add playlist ownership, wip

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,13 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  helper_method :current_user, :most_recent_song
 
   def most_recent_song
     Song.find(session[:most_recent_song_id])
+  end
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
   resources :artists do
     resources :songs, only: [:new, :create]
   end
-
+  get '/auth/spotify/callback', to: 'sessions#create'
+  delete '/logout', 'sessions#destroy'
   resources :songs, only: [:show]
   resources :playlists, only: [:index, :new, :create, :show, :edit, :update]
 end
